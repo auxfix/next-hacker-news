3d8adc20848d69116a1c67a29245c1c29c0e8860
oleg shchepotkin <music.wave.form7@gmail.com>
2024-02-20 15:20:54 +0300

    (style): a bit of tilwind css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import styles from './header.module.scss';
 
 export default function Header() {
   return (
-    <header className={styles.wrapper}>
+    <header className="inset-x-0 top-0 h-28 bg-darkblue fixed z-50">
       <div className={styles.container}>
         <Button onClick={() => console.log('i was clicked')}>Get more news</Button>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'darkblue': '#113740',
     },
   },
   plugins: [],
