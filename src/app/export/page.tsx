a3b51af70b6501d3ec85a0d33e0de35032d5f5e1
oleg shchepotkin <music.wave.form7@gmail.com>
2024-04-05 15:34:56 +0300

    (export): loading indicator

--- src/app/export/page.tsx
+++ src/app/export/page.tsx
@@ -4,9 +4,12 @@ import React, { useState } from 'react';
 
 export default function Home() {
     const [news, setNews] = useState<string>('');
+    const [isLoadingBlob, setIsLoadingBlob] = useState<boolean>(false);
+    const [isLoadingText, setIsLoadingText] = useState<boolean>(false);
 
     async function retrieveFileBlob() {
         try {
+            setIsLoadingBlob(true);
             const ftch = await fetch(
                 `/api/news-export`,
                 {
@@ -21,7 +24,8 @@ export default function Home() {
             link.href = window.URL.createObjectURL(fileBlob)
             link.download = 'hackerNews.json'
             link.click()
-            link.remove();  
+            link.remove();
+            setIsLoadingBlob(false);  
         } catch (e) {
             console.log({ "message": e, status: 400 })  // handle error
         }
@@ -29,6 +33,7 @@ export default function Home() {
 
     async function getNew() {
         try {
+            setIsLoadingText(true);
             const ftch = await fetch(
                 `/api/news-export`,
                 {
@@ -39,7 +44,8 @@ export default function Home() {
                 },
             )
             const { newsItem } = await ftch.json()
-            setNews(JSON.stringify(newsItem, null, 2)); 
+            setNews(JSON.stringify(newsItem, null, 2));
+            setIsLoadingText(false);
         } catch (e) {
             console.log({ "message": e, status: 400 })  // handle error
         }
@@ -50,14 +56,14 @@ export default function Home() {
       <div className='flex h-20 w-full items-center'>
         <h2 className='font-bold text-2xl pl-5 pr-10'>Export tab</h2>
         <button
-            className='bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' 
+            className='bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex' 
             onClick={retrieveFileBlob}>
-                Export top 10 rundom news
+                {isLoadingBlob && <Loading />} Export top 10 rundom news 
         </button>
         <button
-            className='bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10' 
+            className='bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10 flex' 
             onClick={getNew}>
-                Get top 10 news
+               {isLoadingText && <Loading />} Get top 10 news 
         </button>
       </div>
 
@@ -68,4 +74,13 @@ export default function Home() {
         rounded resize-none focus:outline-none focus:bg-white focus:border-blue-500" />
     </main>    
   );
+}
+
+function Loading(){
+    return (<span className="mr-2">
+    <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A8.001 8.001 0 013.707 7.707l2.586 2.586A6 6 0 105.414 17.9l-1.415-1.414z"></path>
+    </svg>
+  </span>)
 }
