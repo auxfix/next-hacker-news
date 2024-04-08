5d91793bdf984fae414ebd2511ae9ef12945705b
oleg shchepotkin <music.wave.form7@gmail.com>
2024-04-09 01:22:34 +0300

    (db): get news from db

--- src/app/api/news-save/route.ts
+++ src/app/api/news-save/route.ts
@@ -1,8 +1,14 @@
-import { saveNews } from "@/query/serverQueries";
+import { saveNews, getNews } from "@/query/serverQueries";
 
 export async function POST(req: any){
   const { news } = await req.json();
   await saveNews(news);
 
   return Response.json('ok')
+}
+
+export async function GET(){
+  const news = await getNews();
+
+  return Response.json(news);
 }
