2c443f43fda7ca5c781fcb43d02dbb921c3cc121
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-06 11:37:13 +0300

    (redux): header

--- src/components/ReduxHeader/index.tsx
+++ src/components/ReduxHeader/index.tsx
@@ -13,7 +13,7 @@ export default function ReduxHeader() {
   return (
     <header className="inset-x-0 top-0 h-28 bg-darkblue fixed z-50">
       <div className={styles.container}>
-        <Button onClick={refetchNews}>Get more news(redux)</Button>    
+        <Button onClick={refetchNews}>Get more news(redux-edit)</Button>    
       </div>
       <div className='absolute top-10 right-12'>
         <Link className='text-white font-bold text-2xl hover:underline' href="/dev">dev</Link>
