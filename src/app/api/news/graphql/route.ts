7b86d2f85ea6ca4b5d3f9016a34c737522e312d4
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-22 20:18:41 +0300

    (gql): firts query

--- src/app/api/news/graphql/route.ts
+++ src/app/api/news/graphql/route.ts
@@ -1,6 +1,40 @@
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { NextRequest } from "next/server";
+import { gql } from "graphql-tag";
+import { getNewsServer } from "@/lib/query/queries";
 
+const typeDefs = gql`
+  type HackerNews {
+    authorId: String
+    id: Int
+    img: String
+    karma: Int 
+    num: Int
+    score: Int
+    time: Int
+    title: String
+    url: String
+  }
+  type Query {
+    news: [HackerNews]
+  }
+`;
 
-export async function GET() {
+const resolvers = {
+  Query: {
+    news: async () => await getNewsServer(),
+  },
+};
 
-    return Response.json({ newsItem: 'hello' })
-}
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+// Typescript: req has the type NextRequest
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+    context: async req => ({ req }),
+});
+
+export { handler as GET, handler as POST };
